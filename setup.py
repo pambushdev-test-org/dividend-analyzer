2153c0c636bbf506a17ffef8127895a3e8db7871
Paris Ambush <paris.ambush@gmail.com>
2022-02-04 17:52:41 -0800

    Update license. Import email dependecies in build.

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
 
+from os import listdir
+from os.path import isfile, join
 from setuptools import setup, find_packages
-
 
 with open('README.rst') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
     license = f.read()
+
+def get_email_dependencies():    
+    path = join('dividend-analyzer', 'email')
+    file_list = [join(path, f) for f in listdir(path) if isfile(join(path, f))]
+    return file_list
 
 setup(
     name='dividend-analyzer',
@@ -18,6 +24,6 @@ setup(
     author_email='paris.ambush@gmail.com',
     url='https://github.com/pambushdev-test-org/dividend-analyzer',
     license=license,
+    data_files=[('email', get_email_dependencies())],
     packages=find_packages(exclude=('tests', 'docs'))
 )
-

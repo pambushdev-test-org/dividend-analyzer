311fcc1cd7d51b0915f643992e29aec47f7426ed
Paris Ambush <paris.ambush@gmail.com>
2022-02-07 17:04:34 -0800

    Update README ref in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 src_dir = 'dividend_analyzer'
 
-with open('README.rst') as f:
+with open('README.md') as f:
     readme = f.read()
 
 with open('LICENSE') as f:
